62c344b79e79874c710cd141164e19ee3495aa96
kanglerian <kanglerian@gmail.com>
2021-03-09 17:36:18 +0700

    pertama

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <p>Tambahannya ini,dihapus!!!</p>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="About" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
